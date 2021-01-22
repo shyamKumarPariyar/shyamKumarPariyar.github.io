cb981b8d99e0e474e71179c5c408f85645cea616
Shyam Kumar Pariyar <aps.sami2013@gmail.com>
2021-01-22 23:25:24 +0545

    fixes in video

--- three.js/src/threex/threex.videotexture.js
+++ three.js/src/threex/threex.videotexture.js
@@ -8,7 +8,7 @@ THREEx.VideoTexture = function(url) {
     video.autoplay = false;
     video.loop = true;
     video.src = url;
-    video.playsInline = true;
+    video.playsinline = true;
     video.webkitPlaysinline = true;
     // expose video as this.video
     this.video = video
