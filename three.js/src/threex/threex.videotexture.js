bac26b9daf2b0cce5a8f5272147f39acf67e05a7
Shyam Kumar Pariyar <aps.sami2013@gmail.com>
2021-01-23 00:15:46 +0545

    fixes in video

--- three.js/src/threex/threex.videotexture.js
+++ three.js/src/threex/threex.videotexture.js
@@ -3,8 +3,8 @@ var THREEx = THREEx || {}
 THREEx.VideoTexture = function(url) {
     // create the video element
     var video = document.createElement('video');
-    // video.width = 320;
-    // video.height = 240;
+    video.width = 640;
+    video.height = 480;
     video.autoplay = false;
     video.loop = true;
     video.src = url;
