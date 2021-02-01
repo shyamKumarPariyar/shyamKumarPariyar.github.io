605397b66d842935a44b664bfe4bec95fff2a515
Shyam Kumar Pariyar <aps.sami2013@gmail.com>
2021-02-01 15:38:36 +0545

    adding debug

--- three.js/src/threex/threex-armarkercontrols.js
+++ three.js/src/threex/threex-armarkercontrols.js
@@ -200,6 +200,8 @@ ARjs.MarkerControls.prototype.name = function() {
     } else {
         console.assert(false, 'no .name() implemented for this marker controls')
     }
+
+    console.log('Hello saam:- ', name)
     return name
 }
 
@@ -262,7 +264,6 @@ ARjs.MarkerControls.prototype._initArtoolkit = function() {
 
     function onMarkerFound(event) {
         // honor his.parameters.minConfidence
-        console.log('Hello saam:- ', this.ARjs.ArMarkerControls)
         if (event.data.type === artoolkit.PATTERN_MARKER && event.data.marker.cfPatt < _this.parameters.minConfidence) return
         if (event.data.type === artoolkit.BARCODE_MARKER && event.data.marker.cfMatrix < _this.parameters.minConfidence) return
 
