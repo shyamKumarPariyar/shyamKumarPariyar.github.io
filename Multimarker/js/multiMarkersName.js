415836589b041e8e7cd1583b91287ead1255c16c
Shyam Kumar Pariyar <aps.sami2013@gmail.com>
2021-02-02 15:41:29 +0545

    adding plane

--- Multimarker/js/multiMarkersName.js
+++ Multimarker/js/multiMarkersName.js
@@ -1,61 +1,63 @@
 //Multi Markers WebAR-AR.js and Aframe - Playing the Archive - Connected Environment CASA-UCL
 
 //Global Variable
-var markersURLArray=[];
-var markersNameArray=[];
+var markersURLArray = [];
+var markersNameArray = [];
 
-AFRAME.registerComponent('markers_start',{
-	init:function(){
-		console.log('Add markers to the scene');
+AFRAME.registerComponent('markers_start', {
+    init: function() {
+        console.log('Add markers to the scene');
 
-		var sceneEl = document.querySelector('a-scene');
-		
-		//list of the markers
-		for(var i=1; i<19; i++)
-		{
-			var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
-			markersURLArray.push(url);
-			markersNameArray.push('Marker_'+i);
-			//console.log(url);
-		}
+        var sceneEl = document.querySelector('a-scene');
 
-		for(var k=0; k<18; k++)
-		{
-			var markerEl = document.createElement('a-marker');
-			markerEl.setAttribute('type','pattern');
-			markerEl.setAttribute('url',markersURLArray[k]);
-			markerEl.setAttribute('id',markersNameArray[k]);
+        //list of the markers
+        for (var i = 1; i < 19; i++) {
+            var url = "resources/markers/pattern-Individual_Blocks-" + i + ".patt";
+            markersURLArray.push(url);
+            markersNameArray.push('Marker_' + i);
+            //console.log(url);
+        }
 
-			markerEl.setAttribute('registerevents','');
-			sceneEl.appendChild(markerEl);
+        for (var k = 0; k < 18; k++) {
+            var markerEl = document.createElement('a-marker');
+            markerEl.setAttribute('type', 'pattern');
+            markerEl.setAttribute('url', markersURLArray[k]);
+            markerEl.setAttribute('id', markersNameArray[k]);
 
-			//Adding text to each marker
-			var textEl = document.createElement('a-entity');
-			
-			textEl.setAttribute('id','text');
-			textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
-			textEl.object3D.position.set(0, 0.7, 0);
-			textEl.object3D.rotation.set(-90, 0, 0);
+            markerEl.setAttribute('registerevents', '');
+            sceneEl.appendChild(markerEl);
 
-			markerEl.appendChild(textEl);
-		}
-	}
+            //Adding text to each marker
+            var planeEl = document.createElement('a-plane');
+
+            planeEl.setAttribute('id', 'plane-'.k);
+            planeEl.setAttribute('width', 1);
+            planeEl.setAttribute('height', 1);
+            planeEl.setAttribute('color', '#7BC8A4');
+            // planeEl.setAttribute('src', '/video.mp4');
+            // textEl.setAttribute('text', { color: 'red', align: 'center', value: markersNameArray[k], width: '5.5' });
+            planeEl.object3D.position.set(0, 0.7, 0);
+            planeEl.object3D.rotation.set(-90, 0, 0);
+
+            markerEl.appendChild(planeEl);
+        }
+    }
 });
 
 
 //Detect marker found and lost
 AFRAME.registerComponent('registerevents', {
-		init: function () {
-			const marker = this.el;
+    init: function() {
+        const marker = this.el;
 
-			marker.addEventListener("markerFound", ()=> {
-				var markerId = marker.id;
-				console.log('Marker Found: ', markerId);
-			});
+        marker.addEventListener("markerFound", () => {
+            var markerId = marker.id;
+            console.log('Marker Found: ', markerId);
+        });
 
-			marker.addEventListener("markerLost",() =>{
-				var markerId = marker.id;
-				console.log('Marker Lost: ', markerId);
-			});
-		},
-	});
+        marker.addEventListener("markerLost", () => {
+            var markerId = marker.id;
+            console.log('Marker Lost: ', markerId);
+        });
+    },
+});
