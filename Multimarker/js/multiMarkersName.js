4dee86744bbc8961d3dfddac5eacb0bf667d4fd9
Shyam Kumar Pariyar <aps.sami2013@gmail.com>
2021-02-02 22:21:27 +0545

    adding markers

--- Multimarker/js/multiMarkersName.js
+++ Multimarker/js/multiMarkersName.js
@@ -35,34 +35,34 @@ AFRAME.registerComponent('markers_start', {
 
             textEl.object3D.position.set(0, 0.7, 0);
             textEl.object3D.rotation.set(-90, 0, 0);
-            sceneEl.appendChild(textEl);
+            markerEl.appendChild(textEl);
 
 
             //Adding video assets to each marker
-            // var assetEl = document.createElement('a-assets');
-            // sceneEl.appendChild(assetEl);
+            var assetEl = document.createElement('a-assets');
+            sceneEl.appendChild(assetEl);
 
             // //Adding video assets to each marker
-            // var videoEl = document.createElement('video');
+            var videoEl = document.createElement('video');
 
-            // videoEl.src = '/video.mp4';
-            // videoEl.setAttribute('id', 'vid_' + k);
-            // videoEl.muted = true
-            // assetEl.appendChild(videoEl);
+            videoEl.src = '/video.mp4';
+            videoEl.setAttribute('id', 'vid_' + k);
+            videoEl.muted = true
+            assetEl.appendChild(videoEl);
 
 
             // //Adding plane to each marker
-            // var planeEl = document.createElement('a-plane');
+            var planeEl = document.createElement('a-plane');
 
-            // planeEl.setAttribute('id', 'plane-' + k);
-            // planeEl.setAttribute('width', 1);
-            // planeEl.setAttribute('height', 1);
-            // planeEl.setAttribute('src', '#vid_' + k);
-            // // planeEl.setAttribute('color', '#000000');
-            // planeEl.object3D.position.set(0, 0.5, 0);
-            // planeEl.object3D.rotation.set(-90, 0, 0);
+            planeEl.setAttribute('id', 'plane-' + k);
+            planeEl.setAttribute('width', 1);
+            planeEl.setAttribute('height', 1);
+            planeEl.setAttribute('src', '#vid_' + k);
+            // planeEl.setAttribute('color', '#000000');
+            planeEl.object3D.position.set(0, 0.5, 0);
+            planeEl.object3D.rotation.set(-90, 0, 0);
 
-            // sceneEl.appendChild(planeEl);
+            markerEl.appendChild(planeEl);
         }
     }
 });
