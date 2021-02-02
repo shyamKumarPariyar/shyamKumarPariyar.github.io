97dd083cf69a5f4254283cd14a6cf656769557df
Shyam Kumar Pariyar <aps.sami2013@gmail.com>
2021-02-02 16:45:31 +0545

    adding plane

--- Multimarker/js/multiMarkersName.js
+++ Multimarker/js/multiMarkersName.js
@@ -11,14 +11,14 @@ AFRAME.registerComponent('markers_start', {
         var sceneEl = document.querySelector('a-scene');
 
         //list of the markers
-        for (var i = 1; i < 19; i++) {
+        for (var i = 1; i < 10; i++) {
             var url = "resources/markers/pattern-Individual_Blocks-" + i + ".patt";
             markersURLArray.push(url);
             markersNameArray.push('Marker_' + i);
             //console.log(url);
         }
 
-        for (var k = 0; k < 18; k++) {
+        for (var k = 0; k < 19; k++) {
             var markerEl = document.createElement('a-marker');
             markerEl.setAttribute('type', 'pattern');
             markerEl.setAttribute('url', markersURLArray[k]);
